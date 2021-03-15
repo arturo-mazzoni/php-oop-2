46baedd9500a9b16909cf82c4fe6f8856a00552f
arturo <arturomazzoni@gmail.com>
2021-03-15 17:52:05 +0100

    set/getsubsriptions

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $user_2->email = 'franco.nari@hotmail.com';
 $user_2->password = 'francofranco72';
 
 $user_2->setSubsriptions(123000);
-$subscriptions = user_2->getSubsriptions();
+$subscriptions = $user_2->getSubsriptions();
 
 ?>
 
